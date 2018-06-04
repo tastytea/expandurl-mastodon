42d1898ce3fca4ae6003532e1ec08e1ec618ed37
tastytea <tastytea@tastytea.de>
2018-06-04 21:04:38 +0200

    git.schlomp.space -> schlomp.space

--- src/masto.cpp
+++ src/masto.cpp
@@ -348,7 +348,7 @@ const bool Listener::register_app()
     ret = _masto->register_app1("expandurl-mastodon",
                                 "urn:ietf:wg:oauth:2.0:oob",
                                 "read write",
-                                "https://git.schlomp.space/tastytea/expandurl-mastodon",
+                                "https://schlomp.space/tastytea/expandurl-mastodon",
                                 client_id,
                                 client_secret,
                                 url);
